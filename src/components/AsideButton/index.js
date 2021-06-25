2f088cd6eb4ec5bdcbef8c413e6339df0a92eef2
Cesar Meza <cmeza@mezadigital.com>
2021-06-24 21:03:44 -0500

    [UPDATE] Adding borders to BotomCard and adding Aside Button Component

---
+++ src/components/AsideButton/index.js
@@ -0,0 +1,25 @@
+import React, {Fragment} from 'react'
+import './styles.css'
+
+const AsideButton = (props) => {
+    return (
+        <>
+            <div >
+                <a href={props.action} className='aside-button__text'>{props.lbl}
+                    <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg" className="aside-button__arrow">
+                        <path d="M15 9L12.885 11.115L19.755 18L12.885 24.885L15 27L24 18L15 9Z"  />
+                    </svg>
+                </a>              
+            </div>
+        </>
+    )
+}
+
+AsideButton.defaultProps={
+    action: '/',
+    lbl: 'Button Label',
+
+  }
+  
+
+export default AsideButton
--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,12 +1,18 @@
 import "./styles.css";
 import Information from '../../components/Feedback'
 import StudentList from '../../components/StudentList'
+import AsideButton from "../../components/AsideButton";
 
 function Dashboard({title, items}) {
   return(
     <>
     <Information />
     <StudentList />
+
+    <AsideButton ></AsideButton>
+    <AsideButton lbl='Editar Perfil' action="/"></AsideButton>
+    <AsideButton lbl='Ver Agenda' action="/"></AsideButton>
+    <AsideButton lbl='Enviar Feedback' action="/"></AsideButton>
     </>
   )
 
