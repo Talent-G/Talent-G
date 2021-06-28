ebb187c68fafe95a45accd26b0f42d4cf6e011cb
tiheryv <tiheryvr@gmail.com>
2021-06-28 16:48:25 -0500

    Added second form

---
+++ src/components/SecondForm/index.js
@@ -0,0 +1,68 @@
+import ReactDom from "react-dom";
+import React, { useState } from "react";
+import "./styles.css";
+import Button from '../Button';
+
+
+
+export default function SecondForm({ title, dayNumber, classTheme }) {
+    const primario = 'primary-button';
+
+    const [description, setDescription] = useState("");
+
+  const enviar = (event) =>{
+    event.preventDefault();
+  
+  }
+  
+  const action = (event) => {
+    event.preventDefault();
+    setDescription(event.target.value)
+  };
+
+console.log(description);
+  
+
+  return (
+    <div >
+      <form className="form_body" onSubmit={enviar}>
+        <div className="label_form">
+          
+          <div className="header">
+            <h1> {title} </h1>
+          </div>
+          
+          <span>Día {dayNumber} - {classTheme} </span>
+          <br></br>
+
+          <label className="label_summary"></label>
+          <br></br>
+          <textarea
+            className="label text_area"
+            placeholder="Summary..."
+            onChange={action}
+            value={description}
+            name="summary"
+          />
+
+          <select>
+              <option value="trainer">Trainer..</option>
+              <option value="Miguel Romero">Miguel Romero</option>
+              <option value="Ruben Pantoja">Rúben Pantoja</option>
+              <option value="Ad Quintero">Ad Quintero</option>
+          </select>
+        
+        </div>
+
+        <Button children='Guardar' tipo={primario} type='submit' />
+      </form>
+    </div>
+  );
+}
+SecondForm.defaultProps = {
+  open: false,
+  classTheme: "CSS",
+  dayNumber: "1",
+  title:"Agenda"
+};
+
