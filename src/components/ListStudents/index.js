d0db981f5200bae69f6390558c5b2e8ecf55d709
Mauricio <mauriciokrm@gmail.com>
2021-06-29 00:03:51 -0500

    Primera version de acciones del modal

--- src/components/ListStudents/index.js
+++ src/components/ListStudents/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './styles.css';
 
-export default function ListStudents({ titulo, list_name }) {
-  const listNameStudets = list_name.map((name) => <li><a href='#'>{name}</a></li>);
+export default function ListStudents({ titulo, listName }) {
+  const listNameStudets = listName.map((name) => <li><a href='/'>{name}</a></li>);
   return (
     <div className='ListStudents'>
       <h2>
@@ -18,5 +18,5 @@ export default function ListStudents({ titulo, list_name }) {
 }
 
 ListStudents.defaultProps = {
-  list_name: ['Alejandra G moreno', 'Alex Camacho', 'Victor Cruz'],
+  listName: ['Alejandra G moreno', 'Alex Camacho', 'Victor Cruz'],
 };
--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import MainInfo from '../../components/MainInfo';
@@ -6,12 +6,37 @@ import InfoProfile from '../../components/InfoProfile';
 import Feedback from '../../components/Feedback';
 import AsideButton from '../../components/AsideButton';
 import Survey from '../../components/Survey';
+import Button from '../../components/Button';
+import Modal from '../../components/Modal';
 
 import './styles.css';
 
 function Dashboard() {
+  const color = ['rgba(0,0,0,0)', 'rgba(54,54,54,0.77)'];
+  const [open, setOpen] = useState(false);
+  const [colorTransparent, setColorTransparent] = useState(color[0]);
+  const [component, setComponent] = useState('');
+  const changePropierties = () => {
+    setOpen(!open);
+    if (open === true) { setColorTransparent(color[0]); }
+    if (open === false) { setColorTransparent(color[1]); }
+  };
+
+  const actionModal = (componentName) => {
+    setComponent(componentName);
+    changePropierties();
+  };
+  const verModal = (component) => {
+    return (
+      <Modal componente={component} open={open}>
+        <Button callback={changePropierties} type='secondary-button'> Cerrar </Button>
+      </Modal>
+    );
+
+  };
+
   return (
-    <div className='dashboard'>
+    <div className='dashboard' style={{ backgroundColor: colorTransparent }}>
       <Header />
       <div className='dashboard__container'>
         <div className='dashboard-grid'>
@@ -22,9 +47,9 @@ function Dashboard() {
             <MainInfo />
           </section>
           <section className='dashboard__buttons wrapper'>
-            <AsideButton lbl='Editar Perfil' />
-            <AsideButton lbl='Ver Agenda' />
-            <AsideButton lbl='Ver Feedback' />
+            <AsideButton lbl='Editar Perfil' action={() => actionModal('Formulario')} />
+            <AsideButton lbl='Ver Agenda' action={() => actionModal('--')} />
+            <AsideButton lbl='Ver Feedback' action={() => actionModal('List_Students')} />
           </section>
           <section className='dashboard__feedback'>
             <Feedback />
@@ -35,6 +60,7 @@ function Dashboard() {
         </div>
       </div>
       <Footer />
+      {verModal(component)}
     </div>
   );
 }
