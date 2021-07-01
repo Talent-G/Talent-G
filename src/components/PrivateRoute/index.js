8dc41cf0e6514b47a7f5512c1a9a705d7bc6a6ad
mariosilvae <mario.silvae@outlook.com>
2021-06-30 23:52:42 -0500

    [feature] private router and page not found

--- src/components/PrivateRoute/index.js
+++ src/components/PrivateRoute/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-export default function PrivateRoute({ exact, path, component: Component }) {
-  const auth = true;
+function PrivateRoute({ exact, path, component: Component, auth }) {
   return (
     <Route exact={exact} path={path}>
       {auth ? <Component /> : <Redirect to='/login' />}
     </Route>
   );
 }
+
+const mapStateToProps = (state) => {
+  return {
+    auth: state?.students?.auth,
+  };
+};
+
+export default connect(mapStateToProps, null)(PrivateRoute);
--- src/routes/App.js
+++ src/routes/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import PrivateRoute from '../components/PrivateRoute';
 import Dashboard from '../containers/Dashboard';
+import NotFound from '../containers/NotFound';
 import Login from '../components/Login';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <PrivateRoute exact path='/' component={Dashboard} />
         <Route exact path='/login' component={Login} />
+        <PrivateRoute exact path='/' component={Dashboard} />
+        <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
   );
