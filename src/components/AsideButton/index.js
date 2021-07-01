6265789e2550e4b6a6851307421f18827ad6dc86
mariosilvae <mario.silvae@outlook.com>
2021-07-01 14:56:31 -0500

    [fix]

--- src/components/AsideButton/index.js
+++ src/components/AsideButton/index.js
@@ -3,11 +3,10 @@ import './styles.css';
 
 const AsideButton = (props) => {
   const { action, lbl } = props;
-
   return (
     <>
-      <div className='aside_button'>
-        <a href={action} className='aside-button__text'>
+      <button onClick={action} type='button' className='aside_button'>
+        <div className='aside-button__text'>
           {lbl}
           <svg
             width='36'
@@ -19,15 +18,15 @@ const AsideButton = (props) => {
           >
             <path d='M15 9L12.885 11.115L19.755 18L12.885 24.885L15 27L24 18L15 9Z' />
           </svg>
-        </a>
-      </div>
+        </div>
+      </button>
     </>
   );
 };
 
 AsideButton.defaultProps = {
-  action: '/',
   lbl: 'Button Label',
 };
 
 export default AsideButton;
+
