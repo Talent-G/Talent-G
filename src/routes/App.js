6d9456559ab6a8ecf278c600bafc907fd15798b0
Mauricio <mauriciokrm@gmail.com>
2021-07-01 12:05:16 -0500

    quite comentarios

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
-// import { BrowserRouter, Route, Switch } from 'react-router-dom';
-// import PrivateRoute from '../components/PrivateRoute';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import PrivateRoute from '../components/PrivateRoute';
 import Dashboard from '../containers/Dashboard';
-// import NotFound from '../containers/NotFound';
-// import Login from '../components/Login';
+import NotFound from '../containers/NotFound';
+import Login from '../components/Login';
 
 function App() {
   return (
-    <Dashboard> </Dashboard>
-    // <BrowserRouter>
-    //   <Switch>
-    //     <Route exact path='/login' component={Login} />
-    //     <PrivateRoute exact path='/' component={Dashboard} />
-    //     <Route component={NotFound} />
-    //   </Switch>
-    // </BrowserRouter>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/login' component={Login} />
+        <PrivateRoute exact path='/' component={Dashboard} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
